45d6fb8aa11d793096b1830a35fbf391b14ed0b7
akinsola <thomas.lawanson@gmail.com>
2021-06-04 13:18:48 +0100

    update

--- src/Models/TextLinkModel.js
+++ src/Models/TextLinkModel.js
@@ -23,7 +23,11 @@ let NETWORK = [
     SPACE,
     "Links",
     SPACE,
-    "This is the fundamental unit of the network. These are the processes/occasions of relational experience, feeling information, that produce nodes.",
+    "This is the fundamental unit of the network.",
+    SPACE,
+    "These are the processes/occasions of relational experience, feeling information, that produce nodes.",
+    SPACE,
+    "Information flows through and is produced by the links.",
     SPACE,
     "Nodes",
     SPACE,
@@ -35,7 +39,60 @@ let NETWORK = [
     SPACE,
     "This feeds into the subjective experience of future nodes", 
     SPACE,
-    "Hence, embodied nodes hold a sequence of abstract images of the mesh or mirror the mesh"
+    "Hence, embodied nodes hold a sequence of abstract images of the mesh or mirror the mesh",
+    "Just as each shell reflects the net of shells in Hidra’s net",
+    SPACE,
+    "Mesh",
+    SPACE,
+    "This is the overall structure and collective desires of the nodes and links embodied in the network.",
+    "Patterns of organisation and organising activity(principles and rules) can be deduced which in turn may define its functionality",
+    "However, the mesh is not static but a dynamic entity formed through the interactions of all the links.",
+    "It has no particular fixed direction and it’s structure can be used to model a non-linear grid of multiple experiences/connections.",
+    "This nonlinear grid model disrupts the chronological time and concepts of causality.",
+    SPACE,
+    "In this framework, there are no pre-supposed nodes or identities but the focus lies on the principles and laws that produce experience which in turn produce dynamic nodes or entities",
+    "The principles or the overall structure are not fundamental laws but are also  autopoietic entities which are informed by the processes of links.",
+    "There is an implicit interdependence between these three units which are in flux.",
+    SPACE,
+    "In this framework, creativity is the only fundamental property of the entities as the dynamic relations between entities (links, nodes, mesh).",
+    "Hence, the future can not be determined through computing previous states as there are no eternal links, nodes or a mesh.",
+    "The purpose of this network framework is to share information. All entities embodied in a network feel and produce information",
+    "Information is a product of the intersecting links. ",
+    "Hence, areas/regions with a high concentration of intersecting links have greater agency in feeling and producing information.",
+    "These areas of high concentration act as centers in a network.",
+    "Networks are polycentric, where physical location is not the fundamental factor that determines a centre but the relative concentration of links.",
+    "The greater the number of links a node has, the more influence the node has on the mesh."
+]
+
+let COMMUNICATION = [
+    "In this text I will be working from Warren Weaver’s definition of communication,",
+    "“communication will be used here in a broad sense to include all of the procedures by which one mind may affect another”.",
+    "Communication through the links will be modelled using Claude Shannon’s model of a communication system.",
+    SPACE,
+    "A communication system consists of essentially 5 parts",
+    SPACE,
+    "Information Source",
+    SPACE,
+    "This is the entity that produces information or sequence of messages to be communicated to a receiving entity.",
+    "In this model of a network, the desires and resulting interactions of intersecting links is the information source.",
+    SPACE,
+    "Transmitter",
+    SPACE,
+    "This transforms the message to find a suitable signal for transition over the channel.",
+    "These are the protocols/rules that define the structure and transmission of information/data/message.",
+    "It can also be thought of as the interpretant.",
+    SPACE,
+    "Channel",
+    SPACE,
+    "This is the medium used to transmit the signal to the receiver.",
+    "The medium affords the structure and the properties of the transmitted message.",
+    SPACE,
+    "Receiver",
+    SPACE,
+    "This converts or interprets the signal to a ‘meaningful’ message for the destination.",
+    "This can also be thought of as the signified."
+
+
 ]
 
 let BURNING_MAN = [
@@ -77,7 +134,47 @@ let BURNING_MAN = [
     "Materialising in the tip-sheets handed out when entering the playa",
     SPACE,
     "An egregore embodying the principles hover the playa™ entangling the self-reliant individuals on their quest for self discovery"
+]
 
+let FREEDOM = [
+    "What does it mean for a node in a network to be free?",
+    SPACE,
+    "Using Isaiah Berlin's two concepts of liberty, I’ll define a framework for looking at the concept of freedom within this network framework.",
+    SPACE,
+    "Negative Freedom is the absence of obstacles, barriers and constraints from outside entities.",
+    "In the realm of political philosophy, it is attributed to individual nodes/agents. ",
+    "It can also be thought of as ‘freedom from’.",
+    "Therefore, entities can’t directly experience this form of freedom yet it allows nodes to act, share and feel information, without obstruction to others.",
+    SPACE,
+    "Positive Freedom is the possibility of acting in such a way that gives one agency to actualise desires.",
+    "It is usually attributed to individual agents. It’s not simply the opportunity to do  but the ability to do.",
+    SPACE,
+    "Entities can only act within the context of their network.",
+    "Therefore, a balance must be struck to create rules which allow for all nodes to act their positive freedoms.",
+    "Whilst, protecting the collective negative freedoms of all nodes.",
+    "These may seem to contradict each other but within this network framework, it’s a matter of access to information. ",
+    "The mesh must respond to the subjective desires of all the nodes.",
+    " Therefore, protocols and rules will need to allow for all nodes to produce and consume meaningful information.",
+
+
+
+]
+
+let POWER = [
+    "“Power is diffusive rather than concentrated,embodied and enacted rather than possessed “.",
+    SPACE,
+    SPACE,
+    "In this framework, power is a function of intersected links and the communication flowing through the links.",
+    "It is the relational agency of entities to actualise desires and communicate/share information to other embodied entities.",
+    "Therefore, It is not an entity wielded by individual nodes/intersecting links but is produced throughout the network through experiences and interactions. ",
+    "It pervades the network and is in constant flux and negotiation due to the dynamic nature of links.",
+    "Nodes use communication(consuming/sharing information) to influence the mesh to reproduce and maintain its own processes.",
+    "Areas with a high concentration of intersecting links have a greater chance of affecting other entities through influencing patterns of organisation and protocols in the mesh.",
+    "This is due to the interdependence of the entities in a network.",
+    "Gramsci’s concept of hegemony is important here as it allows for discussing the role centers of power (areas of high concentration of links) play in imposing knowledge in the communication protocols of a network.",
+    "The concentration of links and their relational power produces a regime of truth or a state of normalcy that produces dynamic laws and protocols. ",
+    "However, the mesh also acts to share collective desires which can be thought of as a passive revolution.",
+    "This accounts for counter-hegemonic struggle for alternative states of normalcy."
 
 ]
 
@@ -110,11 +207,22 @@ MONT_PELERIN = MONT_PELERIN.map((line) => {
     return line.replace(" ", "  ")
 })
 
+COMMUNICATION = COMMUNICATION.map((line) => {
+    return line.replace(" ", "  ")
+})
+
+POWER = POWER.map((line) => {
+    return line.replace(" ", "  ")
+})
+
+FREEDOM = FREEDOM.map((line) => {
+    return line.replace(" ", "  ")
+})
 export const LinkText = {
     NETWORK: NETWORK.join(' '),
-    POWER: '“Power is diffusive rather than concentrated,embodied and enacted rather than possessed “. Power here is a function of intersection of links and the communication flowing through the links. Therefore, it is not an entity wielded by individual nodes.',
-    FREEDOM: 'Negative Freedom is the absence of obstacles, barriers and constraints from outside entities. In liberal tradition, it is attributed to individual agents. It can also be defined as ‘freedom from’. Therefore, one can’t ‘experience’ this form of freedom but it allows one to act (in this model, communicate or feel information) without obstruction to others.',
-    COMMUNICATION: 'A communication system consists of essentially five parts: Information Source, Transmitter, Channel, Receiver, Destination',
+    POWER: POWER.join(' '),
+    FREEDOM: FREEDOM.join(' '),
+    COMMUNICATION: COMMUNICATION.join(' '),
     MONT_PELERIN: MONT_PELERIN.join(' '),
     BURNING_MAN: BURNING_MAN.join(' ')
 }
